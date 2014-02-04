e04692e3a1e3b3797a971ed634dbe762a374af23
winniez <xinyingzengvy@gmail.com>
2014-02-03 17:35:32 -0700

    elegant solution to remove element

--- remove-element.cpp
+++ remove-element.cpp
@@ -11,17 +11,38 @@ Note: we cannot move next element into a cell before checking what the next elem
 [2,2,3] 2, need to handle continous element cases
 
 Solution:
+scan and fill
+maintain 2 pointers will be enough
+
+less elegant----
 maintain 3 pointers
 i - scan pointer
 j - range of identical elment pointer from i
 k - fill pointer
-
 note:
 when jumping out of inner loop, A[j] ! = elem and j is already scan, next time move i to j + 1
 when never inters inner loop, A[i] != elem and copy A[i] to current filling pointer
 
 Time complexity: O(n)
 */
+
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        if (!n) return 0;
+        int cnt = 0;
+        int i = 0, j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (A[i] != elem)
+            {
+                A[j] = A[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
 
 class Solution {
 public:
